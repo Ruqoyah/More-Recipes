18b69515167350c45f31bf4e137bb88a0ab8340f
Ruqoyah <rukayatodukoya123@gmail.com>
2017-09-05 22:54:38 +0100

    Add config file

--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import configs from './../config/config.json';
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
+const config = configs[env];
+
 const db = {};
 
 let sequelize;
-if (env === 'production') {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
-    { dialect: process.env.DB_DIALECT, host: process.env.DB_HOST, port: process.env.DB_PORT });
-} else if (env === 'test') {
-  sequelize = new Sequelize(process.env.DATATBASE_URL);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
-    { dialect: process.env.DB_DIALECT, host: process.env.DB_HOST, port: process.env.DB_PORT });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -30,6 +26,7 @@ fs
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
+
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
