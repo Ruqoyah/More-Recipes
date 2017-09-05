bd1bb8619034044c2a2e1ccf381f424b9d23a2ba
Ruqoyah <rukayatodukoya123@gmail.com>
2017-09-05 21:03:09 +0100

    Add config file

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import winston from 'winston';
-import dotenv from 'dotenv';
 import routes from './server/routes';
 
-dotenv.config();
+
 // Set up the express app
 const app = express();
 
